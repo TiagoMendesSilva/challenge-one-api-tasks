e19f42619504f14a2da438d224b7fb3ba4e924f0
tiagoms <tiagoms@ciandt.com>
2023-06-22 16:21:04 -0300

    Passando os atributos title e description no body da requisição

--- src/server.js
+++ src/server.js
@@ -2,8 +2,21 @@ import http from 'node:http'
 
 const tasks = []
 
-const server = http.createServer((request, response) => {
+const server = http.createServer( async (request, response) => {
     const {method, url} = request
+
+    const buffers = []
+
+    for await (const chunk of request) {
+        buffers.push(chunk)
+    }
+
+    try {
+        request.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        request.body = null
+    }
+    
 
     if(method === 'GET' && url === '/tasks'){
         return response.setHeader('Content-type', 'application/json').end(JSON.stringify(tasks))
@@ -11,10 +24,12 @@ const server = http.createServer((request, response) => {
 
     if(method === 'POST' && url === '/tasks'){
 
+        const {title, description} = request.body
+
         tasks.push({
             id: 1,
-            title: 'Task one',
-            description: 'First task testing',
+            title,
+            description,
             completed_at: null,
             created_at: new Date(),
             updated_at: new Date()
